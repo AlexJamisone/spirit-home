5939fb01bf505980d82a5c4d5de2458d498031fc
AlexJamisone <business.homeit@gmail.com>
2023-04-02 14:32:59 +0300

    if new User, make Cart

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,9 +1,8 @@
 import { clerkClient } from '@clerk/nextjs/server';
 import { TRPCError } from '@trpc/server';
-import { z } from 'zod';
 import { filterUserForClient } from '~/server/helpers/filterUserForClient';
 
-import { createTRPCRouter, publicProcedure, privetProcedure } from '~/server/api/trpc';
+import { createTRPCRouter, privetProcedure } from '~/server/api/trpc';
 
 export const usersRouter = createTRPCRouter({
 	getUser: privetProcedure
@@ -24,6 +23,11 @@ export const usersRouter = createTRPCRouter({
 				await ctx.prisma.user.create({
 					data: {
 						id: ctx.userId,
+					}
+				})
+				await ctx.prisma.cart.create({
+					data: {
+						userId: ctx.userId
 					}
 				})
 			}
