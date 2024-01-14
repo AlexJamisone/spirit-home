1e3de3c91f3832d9893f16fb19154754901e353f
Alex Jamison <business.homeit@gmail.com>
2024-01-14 17:35:59 +0200

    finish base footer

--- src/UI/Footer/index.tsx
+++ src/UI/Footer/index.tsx
@@ -1,11 +1,15 @@
+import { Link } from '@chakra-ui/next-js';
 import { Stack, Text } from '@chakra-ui/react';
+import { useAuth } from '@clerk/nextjs';
 import dayjs from 'dayjs';
 import Instagram from '~/assets/social/Instagram';
 import Telegram from '~/assets/social/Telegram';
 import Youtube from '~/assets/social/Youtube';
+import { socialLinks } from '~/constants/social';
 dayjs().format();
 
 const Footer = () => {
+	const { isSignedIn } = useAuth();
 	return (
 		<Stack
 			as="footer"
@@ -17,13 +21,48 @@ const Footer = () => {
 			zIndex={200}
 			px={20}
 			direction="row"
+			justifyContent="space-between"
+            pt={5}
 		>
-			<Stack direction="row">
-				<Instagram />
-				<Youtube />
-				<Telegram />
+			<Stack>
+				<Link
+					href={isSignedIn ? '/profile/main' : '/signin'}
+					_hover={{
+						textDecoration: 'none',
+					}}
+				>
+					{isSignedIn ? 'Личный кабинет' : 'Войти'}
+				</Link>
+				<Link
+					href={'/favorites'}
+					_hover={{
+						textDecoration: 'none',
+					}}
+				>
+					Избранное
+				</Link>
+				<Link
+					href={'/devilery'}
+					_hover={{
+						textDecoration: 'none',
+					}}
+				>
+					Доставка и оплата
+				</Link>
 			</Stack>
-			<Text>Spirit Home © {dayjs().year()} </Text>
+			<Stack textAlign="center" alignItems="center">
+				<Text fontSize="sm">Все новости о жизни бренда:</Text>
+				<Stack direction="row">
+                    {socialLinks.map((link) => (
+                        <Link href={link.href} key={link.alt} target='_blank'>
+                            <link.child/>
+                        </Link>
+                    ))}
+				</Stack>
+			</Stack>
+			<Stack justifyContent="end">
+				<Text>Spirit Home © {dayjs().year()} </Text>
+			</Stack>
 		</Stack>
 	);
 };
--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,38 +1,28 @@
-import type { IconType } from 'react-icons';
-import { FaTelegramPlane } from 'react-icons/fa';
-import { IoLogoYoutube } from 'react-icons/io';
-import { SlSocialInstagram, SlSocialVkontakte } from 'react-icons/sl';
+import { ElementType } from "react";
+import Instagram from "~/assets/social/Instagram";
+import Telegram from "~/assets/social/Telegram";
+import Youtube from "~/assets/social/Youtube";
 
 type SocialLinksType = {
-	icon: IconType;
+    child: ElementType
 	href: string;
 	alt: string;
-	color: string;
 };
 
 export const socialLinks: SocialLinksType[] = [
+    {
+        alt: 'insta',
+        href: 'https://instagram.com/spirit.h.om.e',
+        child: Instagram,
+    },
 	{
-		icon: IoLogoYoutube,
+		child: Youtube,
 		alt: 'youtube',
 		href: 'https://www.youtube.com/channel/UCi4jdXk-SNNRaiLQK3hNfqA',
-		color: '#E53E3E',
 	},
 	{
 		alt: 'telegram',
 		href: 'https://t.me/spiritom',
-		icon: FaTelegramPlane,
-		color: '#2AABEE',
-	},
-	{
-		alt: 'vk',
-		href: 'https://vk.com/spirit.home',
-		icon: SlSocialVkontakte,
-		color: '#2B6CB0',
-	},
-	{
-		alt: 'insta',
-		href: 'https://instagram.com/spirit.h.om.e',
-		icon: SlSocialInstagram,
-		color: '#FF0080',
+		child: Telegram
 	},
 ];
