87993e592c91e1cfff06c213f7cd03191e993b2b
Alex Jamison <business.homeit@gmail.com>
2024-02-22 17:41:15 +0300

    try other image

--- src/UI/MainSection/MainSection.tsx
+++ src/UI/MainSection/MainSection.tsx
@@ -1,12 +1,14 @@
-import { Image } from '@chakra-ui/next-js';
 import { Stack } from '@chakra-ui/react';
+import Image from 'next/image';
 import MainTitle from '../MainTitle/MainTitle';
 
 const MainSection = () => {
 	return (
-		<Stack width="100%" height="640px" position='relative' mt='-120px' >
+		<Stack width="100%" height="640px" position="relative" mt="-120px">
 			<Image
-				position="absolute"
+				style={{
+					position: 'absolute',
+				}}
 				src="/assets/background.png"
 				alt="main"
 				quality={100}
